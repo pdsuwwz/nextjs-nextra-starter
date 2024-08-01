235d2972170c50e71b869a6fe6a7800a4485ad23
Wisdom <pdsu.wwz@foxmail.com>
2024-08-01 18:47:02 +0800

    feat: setup repo

---
+++ README.md
@@ -0,0 +1,14 @@
+# Nextra Docs Template 
+
+This is a template for creating documentation with [Nextra](https://nextra.site).
+
+
+## Local Development
+
+First, run `pnpm i` to install the dependencies.
+
+Then, run `pnpm dev` to start the development server and visit localhost:3000.
+
+## License
+
+This project is licensed under the MIT License.
---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>My Project</span>,
+  project: {
+    link: 'https://github.com/pdsuwwz/nextjs-nextra-starter',
+  },
+  chat: {
+    link: 'https://discord.com',
+  },
+  docsRepositoryBase: 'https://github.com/pdsuwwz/nextjs-nextra-starter',
+  footer: {
+    text: 'Nextra Docs Template',
+  },
+}
+
+export default config
