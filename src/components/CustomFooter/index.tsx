855e6c9c12cbf4910126ea3fe56bd9fdafd3aad2
Wisdom <pdsu.wwz@foxmail.com>
2024-08-02 18:19:02 +0800

    💅 feat: support tailwind css

---
+++ src/components/CustomFooter/index.tsx
@@ -0,0 +1,10 @@
+export function CustomFooter() {
+  return (
+    <div className="text-[#82959B]">
+      MIT Licensed | Copyright © 2020-
+      { new Date().getFullYear() }
+      {' '}
+      Wisdom.
+    </div>
+  )
+}
---
+++ src/theme.config.tsx
@@ -0,0 +1,63 @@
+import { type DocsThemeConfig, useConfig } from 'nextra-theme-docs'
+import { useRouter } from 'next/router'
+import { CustomFooter } from './components/CustomFooter'
+
+const title = 'My Nextra Starter'
+const description = 'A Starter template with Next.js, Nextra'
+const repo = 'https://github.com/pdsuwwz/nextjs-nextra-starter'
+const url = 'https://github.com/pdsuwwz/nextjs-nextra-starter'
+
+const docsThemeConfig: DocsThemeConfig = {
+  docsRepositoryBase: `${repo}/tree/main/docs`,
+  head: () => <></>,
+  footer: {
+    text: () => (
+      <CustomFooter />
+    ),
+  },
+  logo: () => (
+    <>
+      <span>🚀 My Nextra Starter</span>
+    </>
+  ),
+  primaryHue: 142, // hsl(141.89999999999998, 69.2%, 58%) --tw-green-400
+  project: {
+    link: repo,
+  },
+  themeSwitch: {
+    useOptions: {
+      light: '浅色',
+      dark: '暗黑',
+      system: '跟随系统',
+    },
+  },
+  sidebar: {
+    defaultMenuCollapseLevel: 1,
+  },
+  useNextSeoProps() {
+    const { asPath } = useRouter()
+    const { title: pageTitle } = useConfig()
+    return {
+      additionalLinkTags: [
+        {
+          rel: 'icon',
+          sizes: '20',
+          href: '/vercel-triangle.svg',
+          type: 'image/svg+xml',
+        },
+      ],
+      additionalMetaTags: [],
+      canonical: url,
+      description,
+      openGraph: {
+        title,
+        description,
+        url,
+      },
+      themeColor: 'hsl(141.89999999999998, 69.2%, 58%)',
+      title: asPath !== '/' ? `${pageTitle} - ${title}` : title,
+    }
+  },
+}
+
+export default docsThemeConfig
