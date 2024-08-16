e24b783c90bd01cf7bf30fac4bbefb237e8c4e0e
Wisdom <pdsu.wwz@foxmail.com>
2024-08-16 15:38:45 +0800

    📓 feat: update footer info

--- src/components/CustomFooter/index.tsx
+++ src/components/CustomFooter/index.tsx
@@ -1,10 +1,26 @@
+import Link from 'next/link'
+import { cn } from '@/lib/utils'
+
 export function CustomFooter() {
   return (
-    <div className="text-zinc-600 dark:text-zinc-300/[0.8] font-[inter]">
+    <div className="group text-zinc-600 dark:text-zinc-300/[0.8] font-[inter]">
       MIT Licensed | Copyright © 2020-
       { new Date().getFullYear() }
       {' '}
-      Wisdom.
+      <Link
+        href="https://github.com/pdsuwwz"
+        target="_blank"
+        className={cn(
+          'rounded-none',
+          'hover:text-zinc-900 dark:text-zinc-300',
+          'duration-200 border-b',
+          'border-b-zinc-400/[0.6] hover:border-b-zinc-600',
+          'dark:border-b-zinc-500 dark:hover:border-b-zinc-300',
+        )}
+      >
+        Wisdom
+      </Link>
+      .
     </div>
   )
 }
