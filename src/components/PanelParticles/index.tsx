c8bbddc4951cb5dad0c9ace554b669785d6b9ca7
Wisdom <pdsu.wwz@foxmail.com>
2024-08-20 18:50:53 +0800

    🎃 feat: update hero particles (#25)

---
+++ src/components/PanelParticles/index.tsx
@@ -0,0 +1,90 @@
+import { useTheme } from 'nextra-theme-docs'
+import Particles, { initParticlesEngine } from '@tsparticles/react'
+import type { ISourceOptions } from '@tsparticles/engine'
+import { loadFull } from 'tsparticles'
+import { useEffect, useMemo } from 'react'
+
+const PanelParticles = () => {
+  const { resolvedTheme } = useTheme()
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadFull(engine)
+    })
+  }, [])
+
+
+  const options = useMemo<ISourceOptions>(
+    () => ({
+      fpsLimit: 120,
+      interactivity: {
+        events: {
+          onHover: {
+            enable: true,
+            mode: 'grab',
+          },
+        },
+        modes: {
+          push: {
+            quantity: 4,
+          },
+          repulse: {
+            distance: 200,
+            duration: 0.4,
+          },
+        },
+      },
+      particles: {
+        color: {
+          value: resolvedTheme === 'light' ? '#9f9cbf' : '#c1c7d1',
+        },
+        links: {
+          color: {
+            value: resolvedTheme === 'light' ? '#9f9cbf' : '#c1c7d1',
+          },
+          distance: 120,
+          enable: true,
+          opacity: resolvedTheme === 'light' ? 0.2 : 0.1,
+          width: 1,
+        },
+        move: {
+          direction: 'none',
+          enable: true,
+          outModes: {
+            default: 'bounce',
+          },
+          random: false,
+          speed: 1,
+          straight: false,
+        },
+        number: {
+          density: {
+            enable: true,
+          },
+          value: 60,
+        },
+        opacity: {
+          value: resolvedTheme === 'light' ? 0.2 : 0.15,
+        },
+        shape: {
+          type: 'circle',
+        },
+        size: {
+          value: { min: 1, max: 3 },
+        },
+      },
+      detectRetina: true,
+    }),
+    [resolvedTheme],
+  )
+
+  return (
+    <Particles
+      options={options}
+    />
+  )
+}
+
+export {
+  PanelParticles,
+}
