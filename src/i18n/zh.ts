b839af1ba6fa7873f61fade05d9ea664b54784ab
Wisdom <pdsu.wwz@foxmail.com>
2024-08-21 18:44:08 +0800

    💦 feat: i18n deep complex types keys

--- src/i18n/zh.ts
+++ src/i18n/zh.ts
@@ -6,4 +6,31 @@ export default {
   },
 
   badgeTitle: '轻量级、开箱即用 🎉',
+
+  featureList: [
+    {
+      title: '先进的技术栈',
+      description: '高效的 React 框架和类型安全支持，使用 Next.js、TypeScript、TypeScript、和 Shadcn UI 打造现代化应用',
+    },
+    {
+      title: 'Tailwind CSS & Iconify 图标集',
+      description: '原子化 CSS, 集成 Tailwind CSS 和 Iconify 图标集，轻松实现高效设计、响应式界面 UI',
+    },
+    {
+      title: '暗黑模式',
+      description: '支持暗黑模式，提供更好的夜间使用体验',
+    },
+    {
+      title: '代码规范',
+      description: '遵循最佳实践的代码规范，结合 ESLint 进行代码质量检查与一致性维护',
+    },
+    {
+      title: '丰富组件 & 支持自由扩展',
+      description: '提供丰富的预置组件，并支持灵活的自定义扩展',
+    },
+    {
+      title: '轻量化设计',
+      description: '采用轻量化设计，精简项目设置，专注于内容编写',
+    },
+  ],
 }
