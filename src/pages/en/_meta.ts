6990e4c9ed4284a01956276201cde4e0d8405d9b
Wisdom <pdsu.wwz@foxmail.com>
2024-11-22 16:35:57 +0800

    💠 feat: examples page (#41)

--- src/pages/en/_meta.ts
+++ src/pages/en/_meta.ts
@@ -1,8 +1,16 @@
 export default {
+  '*': {
+    type: 'page',
+  },
   index: {
     display: 'hidden',
     theme: {
       layout: 'raw',
     },
+  },
+  introduction: 'This is Introduction',
+  examples: {
+    title: 'Examples',
+    type: 'page',
   },
 }
