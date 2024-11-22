7e6500a926d524e8bd40705cf782dc8a2dbd5e44
Wisdom <pdsu.wwz@foxmail.com>
2024-11-22 16:32:06 +0800

    💠 feat: examples page

--- src/pages/zh/_meta.ts
+++ src/pages/zh/_meta.ts
@@ -1,8 +1,16 @@
 export default {
+  '*': {
+    type: 'page',
+  },
   index: {
     display: 'hidden',
     theme: {
       layout: 'raw',
     },
+  },
+  introduction: '这是介绍',
+  examples: {
+    title: '示例',
+    type: 'page',
   },
 }
