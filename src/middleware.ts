a20194b315b43d0f0234a2aa4f3dc34ae69e02d0
CeerDecy <79098669+CeerDecy@users.noreply.github.com>
2025-10-29 17:58:11 +0800

    📍 feat: support next sitemap (#91)
    * feat: support next sitemap
    
    * chore: rename next-sitemap.config.ts with suffix mjs and append some newline at the end

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,7 +9,9 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      * - img (image files)
+     * - *.xml (XML files like sitemap.xml, RSS feeds, etc.)
+     * - robots.txt (robots file)
      */
-    '/((?!api|_next/static|_next/image|favicon.ico|img|_pagefind).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico|img|_pagefind|.*\\.xml|robots.txt).*)',
   ],
 }
